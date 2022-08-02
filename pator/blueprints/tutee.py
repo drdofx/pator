65f5fa221cd156f2a0918249a17aed8edb8894c6
Daffa Arviano <daffa.arviano@outlook.com>
2022-08-02 21:16:55 +0700

    add course list and detail views

--- pator/blueprints/tutee.py
+++ pator/blueprints/tutee.py
@@ -8,7 +8,7 @@ from werkzeug.exceptions import abort
 from pator.blueprints.auth import login_required
 from pator.db import get_db
 
-bp = Blueprint('tutee', __name__, url_prefix='/tutee')
+bp = Blueprint('course', __name__, url_prefix='/course')
 # bp_create = Blueprint('create', __name__, url_prefix='/create')
 
 # bp.register_blueprint(bp_create)
@@ -24,7 +24,8 @@ def category(name):
 
     cursor.execute(
         '''
-        SELECT * FROM course c
+        SELECT c.course_name, c.course_prodi, cp.course_rating, cp.hourly_fee, cp.id AS course_tutor_id, u.name
+        FROM course c
         INNER JOIN course_tutor cp ON (c.id = cp.course_id)
         INNER JOIN tutor t ON (cp.tutor_id = t.id)
         INNER JOIN user u ON (t.user_id = u.id)
@@ -36,6 +37,35 @@ def category(name):
     datas = cursor.fetchall()
 
     if datas is not None:
-        return render_template('tutee/menu.html', datas=datas)
+        return render_template('tutee/list.html', datas=datas)
     else:
-        return render_template('tutee/menu.html')
+        return render_template('tutee/list.html')
+
+
+@bp.route('/detail', defaults={'id': None})
+@bp.route('/detail/<int:id>', methods=(['GET']))
+def detail(id):
+    db = get_db()
+    cursor = db.cursor(dictionary=True)
+
+
+    # TODO: Add more inner join to tables course_tutor_keyword and tutor_session_review
+    cursor.execute(
+        '''
+        SELECT c.course_name, c.course_prodi, cp.course_rating, cp.hourly_fee, cp.id AS course_tutor_id, u.name,
+        cp.course_description, t.self_description
+        FROM course_tutor cp
+        INNER JOIN course c ON (cp.course_id = c.id)
+        INNER JOIN tutor t ON (cp.tutor_id = t.id)
+        INNER JOIN user u ON (t.user_id = u.id)
+        WHERE cp.id = %s
+        ''',
+        (id,)
+    )
+
+    data = cursor.fetchone()
+
+    if data is not None:
+        return render_template('tutee/detail.html', data=data)
+    
+    abort(404)
